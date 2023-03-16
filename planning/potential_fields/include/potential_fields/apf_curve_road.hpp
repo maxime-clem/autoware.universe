0af4761888e001f955222334bb5e0c843e96bd97
Maxime CLEMENT <maxime.clement@tier4.jp>
2023-03-16 12:59:00 +0900

    [WIP] cleanup apf_curve_road script, use structs
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- planning/potential_fields/include/potential_fields/apf_curve_road.hpp
+++ planning/potential_fields/include/potential_fields/apf_curve_road.hpp
@@ -15,8 +15,6 @@
 #ifndef POTENTIAL_FIELDS__APF_CURVE_ROAD_HPP_
 #define POTENTIAL_FIELDS__APF_CURVE_ROAD_HPP_
 
-#include <stdio.h>
-
 #include <cmath>
 #include <iostream>
 #include <vector>
@@ -34,6 +32,20 @@ const double c0_llc = 3.5;      // polynomial coefficients of curve road: lower 
 const double delta_Xh = 1e-10;  // additional term in case infinity
 const double sigma_y = 0.865;   // simplify the lateral calc.
 
+struct Info
+{
+  double x;
+  double y;
+  double v_lat;
+  double v_lon;
+  double a_lat;
+  double a_lon;
+  double yaw;
+  double m;
+  double length;
+  double width;
+};
+
 int sign(const double x)
 {
   if (x > 0)
@@ -44,75 +56,48 @@ int sign(const double x)
     return -1;
 }
 
-double getRoadPF(double Ego_info[])
+double getRoadPF(const Info & ego_info)
 {
-  // Ego_info[7]={X_ego,Y_ego,Ve_lat,Ve_long,Ae_lat,Ae_long,Yaw_ego,M_ego,Le_l,Le_w}
-  // extract info
-  double X_ego = Ego_info[0];
-  double Y_ego = Ego_info[1];
   // right lane
-  double m_y = -1 / (2 * c2 * (X_ego + delta_Xh) + c1);
-  double Y_rlc_cr = c2 * std::pow(X_ego + delta_Xh, 2) + c1 * (X_ego + delta_Xh) + c0_rlc;
-  double b_y = Y_rlc_cr - m_y * (X_ego + delta_Xh);
+  double m_y = -1 / (2 * c2 * (ego_info.x + delta_Xh) + c1);
+  double Y_rlc_cr = c2 * std::pow(ego_info.x + delta_Xh, 2) + c1 * (ego_info.x + delta_Xh) + c0_rlc;
+  double b_y = Y_rlc_cr - m_y * (ego_info.x + delta_Xh);
   double U_rl_cr = A_r * std::pow(
                            1 - std::exp(
-                                 -b_r * sign(Y_ego - Y_rlc_cr) *
+                                 -b_r * sign(ego_info.y - Y_rlc_cr) *
                                  std::sqrt(
-                                   std::pow((Y_ego - b_y) / m_y - (X_ego + delta_Xh), 2) +
-                                   std::pow(Y_rlc_cr - Y_ego, 2))),
+                                   std::pow((ego_info.y - b_y) / m_y - (ego_info.x + delta_Xh), 2) +
+                                   std::pow(Y_rlc_cr - ego_info.y, 2))),
                            2);
   // left lane
-  double Y_llc_cr = c2 * std::pow(X_ego + delta_Xh, 2) + c1 * (X_ego + delta_Xh) + c0_llc;
-  double b_y_ll = Y_llc_cr - m_y * (X_ego + delta_Xh);
-  double U_ll_cr = A_r * std::pow(
-                           1 - std::exp(
-                                 b_r * sign(Y_ego - Y_llc_cr) *
-                                 std::sqrt(
-                                   std::pow((Y_ego - b_y_ll) / m_y - (X_ego + delta_Xh), 2) +
-                                   std::pow(Y_llc_cr - Y_ego, 2))),
-                           2);
+  double Y_llc_cr = c2 * std::pow(ego_info.x + delta_Xh, 2) + c1 * (ego_info.x + delta_Xh) + c0_llc;
+  double b_y_ll = Y_llc_cr - m_y * (ego_info.x + delta_Xh);
+  double U_ll_cr =
+    A_r * std::pow(
+            1 - std::exp(
+                  b_r * sign(ego_info.y - Y_llc_cr) *
+                  std::sqrt(
+                    std::pow((ego_info.y - b_y_ll) / m_y - (ego_info.x + delta_Xh), 2) +
+                    std::pow(Y_llc_cr - ego_info.y, 2))),
+            2);
   // total
   double U_cr = U_rl_cr + U_ll_cr;
 
   return U_cr;
 }
 
-double getObsPF(double Ego_info[], double Obs_info[])
+double getObsPF(const Info & ego_info, const Info & obs_info)
 {
-  // extract info
-  // ego vehicle
-  double X_ego = Ego_info[0];
-  double Y_ego = Ego_info[1];
-  double Ve_lat = Ego_info[2];
-  double Ve_long = Ego_info[3];
-  double Ae_lat = Ego_info[4];
-  double Ae_long = Ego_info[5];
-  double Yaw_ego = Ego_info[6];
-  double M_ego = Ego_info[7];
-  double Le_l = Ego_info[8];
-  double Le_w = Ego_info[9];
-
-  // obstacle vehicle
-  double X_obs = Obs_info[0];
-  double Y_obs = Obs_info[1];
-  double Vo_lat = Obs_info[2];
-  double Vo_long = Obs_info[3];
-  double Ao_lat = Obs_info[4];
-  double Ao_long = Obs_info[5];
-  double Yaw_obs = Obs_info[6];
-  double M_obs = Obs_info[7];
-  double Lo_l = Obs_info[8];
-  double Lo_w = Obs_info[9];
-
   // basic parameter setting
-  int F_m = 300;  // brake force of each tire
+  constexpr double F_m = 300.0;  // brake force of each tire
   double x_sigma =
-    (M_ego * (Ve_long * Ve_long - Vo_long * Vo_long)) / (8 * F_m) + (Le_l + Lo_l) / 2;
+    (ego_info.m * (ego_info.v_lon * ego_info.v_lon - obs_info.v_lon * obs_info.v_lon)) / (8 * F_m) +
+    (ego_info.length + obs_info.length) / 2;
 
   // obstacle PF
   double sigma_x = std::sqrt(-pow(x_sigma, 2) / (2 * log(0.01 / A_o)));
-  double psi_obs_tem =
-    std::atan(2 * c2 * X_obs + c1);  // estimated value if ego can't get the obstacle's yaw info
+  double psi_obs_tem = std::atan(
+    2 * c2 * obs_info.x + c1);  // estimated value if ego can't get the obstacle's yaw info
   double a_psi = std::pow(std::cos(psi_obs_tem), 2) / (2 * sigma_x * sigma_x) +
                  std::pow(std::sin(psi_obs_tem), 2) / (2 * sigma_y * sigma_y);
   double b_psi =
@@ -120,10 +105,10 @@ double getObsPF(double Ego_info[], double Obs_info[])
       std::sin(2 * psi_obs_tem) / (4 * sigma_y * sigma_y));
   double c_psi = std::pow(std::sin(psi_obs_tem), 2) / (2 * std::pow(sigma_x, 2)) +
                  std::pow(std::cos(psi_obs_tem), 2) / (2 * std::pow(sigma_y, 2));
-  double U_obs =
-    A_o * std::exp(
-            -(a_psi * std::pow(X_ego - X_obs, 2) + 2 * b_psi * (X_ego - X_obs) * (Y_ego - Y_obs) +
-              c_psi * std::pow(Y_ego - Y_obs, 2)));
+  double U_obs = A_o * std::exp(
+                         -(a_psi * std::pow(ego_info.x - obs_info.x, 2) +
+                           2 * b_psi * (ego_info.x - obs_info.x) * (ego_info.y - obs_info.y) +
+                           c_psi * std::pow(ego_info.y - obs_info.y, 2)));
   return U_obs;
 }
 
@@ -138,7 +123,7 @@ std::vector<double> Fren2Cart(
   double Kappa_r_dot = ref_path[4];
 
   // calculate cartesian frame
-  std::vector<double> Carte_info;
+  std::vector<double> Carte_info(6);
   double X = X_r - l * std::sin(Theta_r);
   double Y = Y_r - l * std::cos(Theta_r);
   double Theta = std::atan(l_dot / (1 - Kappa_r * l)) + Theta_r;
@@ -160,12 +145,5 @@ std::vector<double> Fren2Cart(
   Carte_info[5] = Ax;
   return Carte_info;
 }
-
-int main()
-{
-  std::cout << pow(64.5, 2) << std::endl;
-  return 0;
-}
-
 }  // namespace potential_fields
 #endif  // POTENTIAL_FIELDS__APF_CURVE_ROAD_HPP_
